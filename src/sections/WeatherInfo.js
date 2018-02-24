8fcec353a529dc8821dea9d687fcb37d417d87e3
SebastianGerS <sebastian.gerstel.sollerman@chasacademy.se>
2018-02-24 02:12:48 +0100

    add suport for setting up array of upcomming week and looping thrue the array creating new instances of components for each interation

--- src/sections/WeatherInfo.js
+++ src/sections/WeatherInfo.js
@@ -8,14 +8,79 @@ class WeatherInfo extends Component {
     this.state = {
       lat: undefined, 
       lon: undefined,
+      selectedTempUnit: `${String.fromCharCode(176)}C`,
       currentDay: {
         temp: undefined,
         windSpeed: undefined,
         humidity: undefined,
         sunrise: undefined,
         sunset: undefined,
-        selectedTempUnit: `${String.fromCharCode(176)}C`,
       },
+      week: [
+        {
+          date: undefined,
+          tempMin: undefined,
+          tempMax: undefined,
+          windSpeed: undefined,
+          humidity: undefined,
+          sunrise: undefined,
+          sunset: undefined,
+        },
+        {
+          date: undefined,
+          tempMin: undefined,
+          tempMax: undefined,
+          windSpeed: undefined,
+          humidity: undefined,
+          sunrise: undefined,
+          sunset: undefined,
+        },
+        {
+          date: undefined,
+          tempMin: undefined,
+          tempMax: undefined,
+          windSpeed: undefined,
+          humidity: undefined,
+          sunrise: undefined,
+          sunset: undefined,
+        },
+        {
+          date: undefined,
+          tempMin: undefined,
+          tempMax: undefined,
+          windSpeed: undefined,
+          humidity: undefined,
+          sunrise: undefined,
+          sunset: undefined,
+        },
+        {
+          date: undefined,
+          tempMin: undefined,
+          tempMax: undefined,
+          windSpeed: undefined,
+          humidity: undefined,
+          sunrise: undefined,
+          sunset: undefined,
+        },
+        {
+          date: undefined,
+          tempMin: undefined,
+          tempMax: undefined,
+          windSpeed: undefined,
+          humidity: undefined,
+          sunrise: undefined,
+          sunset: undefined,
+        },
+        {
+          date: undefined,
+          tempMin: undefined,
+          tempMax: undefined,
+          windSpeed: undefined,
+          humidity: undefined,
+          sunrise: undefined,
+          sunset: undefined,
+        },
+      ],
     }
   }
     
@@ -28,11 +93,20 @@ class WeatherInfo extends Component {
       this.getWeatherData();
     }
   }
+  
 
   render() {
+    const week = [];
+    for(let i = 0; i < 7; i++) {
+      let bar = <WeatherBar current= {this.state.week[i]} />
+      week.push(bar);
+    }
+    
+
     return (
       <section className="weatherInfo">
        <WeatherBar current={this.state.currentDay} />
+       {week}
       </section>
     );
   }
@@ -57,17 +131,34 @@ class WeatherInfo extends Component {
     fetch(proxyUrl + url
     ).then(res => res.json()
     ).then(data => {
-      console.log(data);
+      console.log(data)
       this.setState({ 
         currentDay: { 
-          temp: `${data.currently.temperature} ${this.state.currentDay.selectedTempUnit}`,
+          temp: `${data.currently.temperature} ${this.state.selectedTempUnit}`,
           windSpeed: `${data.currently.windSpeed} m/s`,
           humidity: `${data.currently.humidity * 100}%`,
           sunrise: new Date(data.daily.data[0].sunriseTime * 1000).toLocaleTimeString(),
           sunset: new Date(data.daily.data[0].sunsetTime *1000).toLocaleTimeString(),
         },
-      })
-      
+      });
+      data.daily.data.forEach(day => {
+        let weekDay = new Date(day.time * 1000).getDay();
+        this.setState({
+            week: {
+              ...this.state.week,
+              [`${weekDay}`]: {
+                date: day.time,
+                tempMin: `${day.temperatureMin} ${this.state.selectedTempUnit}`,
+                tempMax: `${day.temperatureMax} ${this.state.selectedTempUnit}`,
+                windSpeed: `${day.windSpeed} m/s`,
+                humidity: `${day.humidity * 100}%`,
+                sunrise: new Date(day.sunriseTime * 1000).toLocaleTimeString(),
+                sunset: new Date(day.sunsetTime *1000).toLocaleTimeString(),
+              }
+            }
+        });
+      });
+      console.log(this.state.week);
     }).catch(error => {
       console.log(error);
     });
