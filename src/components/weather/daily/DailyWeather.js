2ea0c22389e864458660233cd28deeaa5cc7a8fe
SebastianGerS <sebastian.gerstel.sollerman@chasacademy.se>
2018-02-25 22:38:01 +0100

    fix mobile layout

--- src/components/weather/daily/DailyWeather.js
+++ src/components/weather/daily/DailyWeather.js
@@ -7,29 +7,33 @@ class DailyWeather extends Component {
     return (
       <section className="dailyWeather">
         <div>
-          <h3>{this.props.day.weekday}</h3>
-          <p> {this.props.day.summary} </p>
+          <div>
+            <h3>{this.props.day.weekday}</h3>
+            <p> {this.props.day.summary} </p>
+          </div>
+          <div>
+            <h4>Temperature</h4>
+            <p>Max:&nbsp;&nbsp;{this.props.day.tempMax}</p>
+            <p> Min:&nbsp;&nbsp;{this.props.day.tempMin}</p>
+          </div>
         </div>
         <div>
-          <h4>Temperature</h4>
-          <p>Max:&nbsp;&nbsp;{this.props.day.tempMax}</p>
-          <p> Min:&nbsp;&nbsp;{this.props.day.tempMin}</p>
-        </div>
-        <div>
-          <p>
-            wind speed:&nbsp;&nbsp;{this.props.day.windSpeed}
-          </p>
-          <p>
-            relative humitity:&nbsp;&nbsp;{this.props.day.humidity}
-          </p>
-        </div>
-        <div>
-          <p>
-            sunrise:&nbsp;&nbsp;{this.props.day.sunrise}
-          </p>
-          <p>
-            sunset:&nbsp;&nbsp;{this.props.day.sunset}
-          </p>
+          <div>
+            <p>
+              wind speed:&nbsp;&nbsp;{this.props.day.windSpeed}
+            </p>
+            <p>
+              relative humitity:&nbsp;&nbsp;{this.props.day.humidity}
+            </p>
+          </div>
+          <div>
+            <p>
+              sunrise:&nbsp;&nbsp;{this.props.day.sunrise}
+            </p>
+            <p>
+              sunset:&nbsp;&nbsp;{this.props.day.sunset}
+            </p>
+          </div>
         </div>
       </section>
     );
